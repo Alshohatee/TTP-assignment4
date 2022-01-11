cb89b59f346726f58fbf18416a9cba1fa67c1ec2
alshohatee <alshohateeaseel@gmail.com>
2022-01-10 23:23:34 -0500

    create big button and send alert when hover over it

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ function button2Click() {
     spanText1.textContent = ""
 
 }
+
+
+//  2. Create an HTML page with a large element on the page that says "Don't hover over me" inside of it.
+//      When you hover over the element, send an alert to the user that says, "Hey, 
+//      I told you not to hover over me! 
+
+let bigBtn = document.getElementById("big-btn")
+
+function mouseOver() {
+    alert("Hey, I told you not to hover over me!")
+}
